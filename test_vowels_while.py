aaadd430ff91cf4804b9cbae19cb48fa2cf94742
manishabheemanpally <143884539+manishabheemanpally@users.noreply.github.com>
2025-01-22 14:40:54 +0530

    Create test_vowels_while.py

---
+++ test_vowels_while.py
@@ -0,0 +1,27 @@
+import unittest
+from vowels_while import count_vowels  # Make sure the import path is correct
+
+class TestCountVowels(unittest.TestCase):
+    def test_empty_string(self):
+        self.assertEqual(count_vowels(""), 0)
+
+    def test_no_vowels(self):
+        self.assertEqual(count_vowels("bcdfgh"), 0)
+
+    def test_all_vowels(self):
+        self.assertEqual(count_vowels("aeiou"), 5)
+        self.assertEqual(count_vowels("AEIOU"), 5)
+
+    def test_mixed_case(self):
+        self.assertEqual(count_vowels("aEiOu"), 5)
+
+    def test_mixed_characters(self):
+        self.assertEqual(count_vowels("hello world"), 3)
+        self.assertEqual(count_vowels("Python Programming"), 4)
+
+    def test_numbers_and_special_chars(self):
+        self.assertEqual(count_vowels("123456!@#"), 0)
+        self.assertEqual(count_vowels("vow3ls!aEioU"), 5)
+
+if __name__ == '__main__':
+    unittest.main()
